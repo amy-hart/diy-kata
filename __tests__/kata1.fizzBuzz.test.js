5173b64bd676ff7f6d1d477c1628d19a5e905436
Amy Hart <amy@pageplay.com>
2018-06-26 15:59:10 +0100

    Completed Kata 1

--- __tests__/kata1.fizzBuzz.test.js
+++ __tests__/kata1.fizzBuzz.test.js
@@ -2,18 +2,18 @@ const { fizzBuzz } = require('../src');
 
 describe('fizzBuzz', () => {
   it('returns Fizz when passed a multiple of 3', () => {
-
+  	expect(fizzBuzz(12)).toBe('Fizz');
   });
 
   it('returns Buzz when passed a multiple of 5', () => {
-
+	expect(fizzBuzz(25)).toBe('Buzz');
   });
 
   it('returns FizzBuzz when passed a multiple 3 and 5', () => {
-
+	expect(fizzBuzz(15)).toBe('FizzBuzz');
   });
 
   it('returns the number when it isn\'t a multiple of 3 or 5', () => {
-
+	expect(28).toBe(28);
   });
 });
--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,17 @@
 const fizzBuzz = (number) => {
-
+	if(number % 3 === 0 && number % 5 === 0){
+		return "FizzBuzz";
+	}else if(number % 3 === 0){
+		// If there is no remainder when the number is divided by 3, it must be a multiple of 3
+		//returns Fizz when passed a multiple of 3
+		return "Fizz";
+	}else if(number % 5 === 0){
+		// If there is no remainder when the number is divided by 5, it must be a multiple of 5
+		//returns Buzz when passed a multiple of 5
+		return "Buzz";
+	}else{
+		return number;
+	}
 }
 
 module.exports = fizzBuzz;
